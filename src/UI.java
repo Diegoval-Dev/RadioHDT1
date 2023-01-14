f3ba475472835ec06bf8ca398633614af1e163d9
Ruth Anaí de León <dele22428@uvg.edu.gt>
2023-01-13 21:18:51 -0600

    Se desarrolló un scanner con menu

--- src/UI.java
+++ src/UI.java
@@ -4,7 +4,13 @@ public class UI {
 
     public static int initial(){
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Seleccione el modo a implementar");
+        System.out.println("");
+        System.out.println("1. Encender radio");
+        System.out.println("2. Cambiar de emisora AM o FM");
+        System.out.println("3. Cambiar de emisora");
+        System.out.println("4. Seleccionas emisora");
+        System.out.println("5. Apagar radio");
         return scanner.nextInt();
     }
 
