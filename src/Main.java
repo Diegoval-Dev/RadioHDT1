5ad72edea156ec59aede6a844e835141f583977d
Ruth Anaí de León <dele22428@uvg.edu.gt>
2023-01-14 08:55:10 -0600

    Se creo el método Status para mandar información junto al UI

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
+
+    public static ClassRadio classR = new ClassRadio();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        while(true){
+
+        }
     }
+
+
+
+
+
+    public static String Status() {
+            String estado2;
+            if (classR.isOn()) {
+                estado2 = "ESTADO: ENCENDIDO";
+            } else {
+                estado2 = "ESTADO: APAGADO";
+            }
+            return estado2;
+    }
+
+
 }
