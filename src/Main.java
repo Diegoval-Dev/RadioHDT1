e8faaeb0edcaeb24ddfb912b8bd44eab2b399375
Ruth Anaí de León <dele22428@uvg.edu.gt>
2023-01-14 08:57:14 -0600

    Se implemento el método menu para desarrollar el menu en UI

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,32 @@ public class Main {
     public static ClassRadio classR = new ClassRadio();
     public static void main(String[] args) {
         while(true){
-
+            menu();
         }
     }
 
+    public static void menu(){
+        UI.show(Status());
+        int opcion = UI.initial();
+        switch(opcion){
+            case 1:
+                classR.isOn();
+                UI.show(Status());
+                break;
+            case 2:
+                classR.isOn();
+                    UI.show("SE CAMBIO A FM");
+                break;
 
+
+
+
+
+
+
+
+        }
+    }
 
 
 
